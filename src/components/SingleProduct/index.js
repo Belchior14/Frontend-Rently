b6693e67c74f148cd5d7afe6fbb16c37a30c65fd
Belchior <belchior.fontao@gmail.com>
2022-04-17 19:02:08 +0100

    added the user of the product at the SingleProduct component

--- src/components/SingleProduct/index.js
+++ src/components/SingleProduct/index.js
@@ -1,8 +1,12 @@
 import { useState, useEffect } from "react";
 import { client } from "client";
+import { Link } from "react-router-dom";
+
 
 export function SingleProductShow() {
   const [product, setProduct] = useState(null);
+  const [userOfTheProduct, setUserOfTheProduct] = useState(null)
+
 
   const oneProduct = async () => {
     try {
@@ -11,6 +15,8 @@ export function SingleProductShow() {
       
       );
       setProduct(response.data.product);
+      setUserOfTheProduct(response.data.rentUser)
+      
     } catch (error) {
       console.log(error);
     }
@@ -19,12 +25,15 @@ export function SingleProductShow() {
   useEffect(() => {
     oneProduct();
   }, []);
-
+  
   return (
+    
     <div>
       {product ? (
         <div>
+          {console.log(userOfTheProduct)}
           <h3>{product.name}</h3>
+          {/* <h1>{userOfTheProduct.firstName}</h1> */}
           <h3>{product.price}</h3>
         </div>
       ) : null}
