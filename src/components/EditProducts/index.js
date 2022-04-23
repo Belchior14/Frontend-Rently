1fdbe4c24bfb70f9c0c0951ffe716ecf6ba2abf5
Belchior <belchior.fontao@gmail.com>
2022-04-23 09:50:43 +0100

    change product content

--- src/components/EditProducts/index.js
+++ src/components/EditProducts/index.js
@@ -1,67 +1,142 @@
 import { client } from "client";
-import { useState ,useEffect } from "react";
+import { AuthContext } from "context";
+import { useState, useEffect, useContext } from "react";
+import { useNavigate } from "react-router-dom";
 
 export function EditProductForm() {
-    const [product, setProduct] = useState()
-    const [newName, setNewName] = useState()
-    const [newDescription, setNewDescription] = useState()
-    const [newImage, setNewImage] = useState()
-    const [newPrice, setNewPrice] = useState()
-    const [newCity, setNewCity] = useState()
-    const [newCountry, setNewCountry] = useState()
+  const navigate = useNavigate();
+  const { user } = useContext(AuthContext);
+  const [product, setProduct] = useState();
+  const [newCategory , setNewCategory] = useState()
+  const [newName, setNewName] = useState();
+  const [newDescription, setNewDescription] = useState();
+  const [newImage, setNewImage] = useState();
+  const [newPrice, setNewPrice] = useState();
+  const [newCity, setNewCity] = useState();
+  const [newCountry, setNewCountry] = useState();
 
-    const oneProduct = async () => {
-        try {
-          const response = await client.get(
-            `/product/${window.location.href.split("/").at(-1)}`
-          );
-          setProduct(response.data.product);
-        } catch (error) {
-          console.log(error);
-        }
-      };
-      useEffect(() => {
-        oneProduct();
-      }, []);
+  const oneProduct = async () => {
+    try {
+      const response = await client.get(
+        `/product/${window.location.href.split("/").at(-1)}`
+      );
+      setProduct(response.data.product);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+ 
+  const saveProduct = async () => {
 
-      const changeProductContent = () => {
-        product &&
-        setNewName(product.name)
-      };
-      useEffect(() => {
-        changeProductContent();
-      }, [product])
+    try{
+
+      const response = await client.put(`product/edit/${window.location.href.split("/").at(-1)}`,
+      {
+        category: newCategory,
+        name: newName,
+        description: newDescription,
+        image: newImage,
+        price: newPrice,
+        city: newCity,
+        country: newCountry
+      })
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+
+  const handleSave = async (e) => {
+    e.preventDefault();
+    saveProduct()
+    navigate(`/profile/${user._id}`)
+  };
+
+  useEffect(() => {
+    oneProduct();
+  }, []);
+
+  const changeProductContent = () => {
+    product && 
+    setNewCategory(product.category)
+    product && 
+    setNewName(product.name)
+    product && 
+    setNewPrice(product.price)
+    product && 
+    setNewDescription(product.description)
+    product && 
+    setNewImage(product.image)
+    product && 
+    setNewCity(product.city)
+    product && 
+    setNewCountry(product.country)
+   
+  };
+  useEffect(() => {
+    changeProductContent();
+  }, [product]);
 
   return (
     <div>
-      {product && 
-      <form>
-          {console.log(product)}
-        <label>Category:</label>
-        <select>
-          <option value="Technology">Technology</option>
-          <option value="Sports">Sports</option>
-          <option option="Home">Home</option>
-          <option option="Leisure">Leisure</option>
-          <option option="Others">Others</option>
-        </select>
+      {product && (
+        <form onSubmit={handleSave}>
+          <label>Category:</label>
+          <select value={newCategory} onChange={(e) => setNewCategory(e.target.value)}>
+            <option value="Technology">Technology</option>
+            <option value="Sports">Sports</option>
+            <option option="Home">Home</option>
+            <option option="Leisure">Leisure</option>
+            <option option="Others">Others</option>
+          </select>
 
-        <label>Product Name:</label>
-        <input type="text" id="name" value={newName} onChange={(event) => setNewName(event.target.value)}/>
-        <label>Description:</label>
-        <input type="text" id="description" value={newDescription} onChange={(event) => setNewDescription(event.target.value)} />
-        <label>Image:</label>
-        <input type="text" id="image" value={newImage} onChange={(event) => setNewImage(event.target.value)}/>
-        <label>Price:</label>
-        <input type="number" id="price" value={newPrice} onChange={(event) => setNewPrice(event.target.value)}/>
-        <label>City:</label>
+          <label>Product Name:</label>
+          <input
+            type="text"
+            id="name"
+            value={newName}
+            onChange={(event) => setNewName(event.target.value)}
+          />
+          <label>Description:</label>
+          <input
+            type="text"
+            id="description"
+            value={newDescription}
+            onChange={(event) => setNewDescription(event.target.value)}
+          />
+          <label>Image:</label>
+          <input
+            type="text"
+            id="image"
+            value={newImage}
+            onChange={(event) => setNewImage(event.target.value)}
+          />
+          <label>Price:</label>
+          <input
+            type="number"
+            id="price"
+            value={newPrice}
+            onChange={(event) => setNewPrice(event.target.value)}
+          />
+          <label>City:</label>
 
-        <input type="text" id="city" value={newCity} onChange={(event) => setNewCity(event.target.value)}/>
-        <label>Country:</label>
-        <input type="text" id="country" value={newCountry} onChange={(event) => setNewCountry(event.target.value)}/>
-        <button type="submit">Edit Product</button>
-      </form>
-   }
-    </div> 
+          <input
+            type="text"
+            id="city"
+            value={newCity}
+            onChange={(event) => setNewCity(event.target.value)}
+          />
+          <label>Country:</label>
+          <input
+            type="text"
+            id="country"
+            value={newCountry}
+            onChange={(event) => setNewCountry(event.target.value)}
+          />
+          <button type="submit">Edit Product</button>
+        </form>
+      )}
+    </div>
   );
 }
