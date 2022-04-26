c2a3f42dd60359afab1548380c386252afad14c2
Belchior <belchior.fontao@gmail.com>
2022-04-26 20:23:44 +0100

    search bar completed

--- src/components/AllProducts/index.js
+++ src/components/AllProducts/index.js
@@ -3,55 +3,42 @@ import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 export function AllProductsShow() {
-  const { search } = window.location;
-/*   const query = new URLSearchParams(search).get("s");
- */  const [searchQuery, setSearchQuery] = useState("");
- const [searchCity, setSearchCity] = useState("")
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searchCity, setSearchCity] = useState("");
   const [products, setProducts] = useState([]);
 
   const getProducts = () => {
     client
       .get("/product")
       .then((response) => {
-        /* console.log(response.data.message) */
         setProducts(response.data.product);
       })
       .catch((error) => console.log(error));
   };
 
-  useEffect(async () => {
-    await getProducts();
-    filterProducts()
+  useEffect(() => {
+    getProducts();
+    filterProducts();
   }, []);
 
-  useEffect(() => {
+  /*   useEffect(() => {
     filterProducts();
-  }, [searchQuery, searchCity])
+  }, [ searchQuery, searchCity ]); */
 
   const filterProducts = () => {
-   /*  if (!query) {
-      return products;
-    } */
-  const productFilters = products;
+    let productFilters = products;
 
-    if (searchQuery) productFilters = productFilters.filter((product) => product.name.includes(searchQuery))
-    if (searchCity) productFilters = productFilters.filter((product) => product.city.includes(searchCity))
+    if (searchQuery)
+      productFilters = productFilters.filter((product) =>
+        product.name.includes(searchQuery)
+      );
+    if (searchCity)
+      productFilters = productFilters.filter((product) =>
+        product.city.includes(searchCity)
+      );
     return productFilters;
-  }; 
+  };
 
-/*   const filteredProducts = filterProducts(products, searchQuery);
- */ /*  const filteredCities = (filteredProducts, query) => {
-    if (!query) {
-      return filteredProducts;
-    } 
-    return filteredProducts.filter((product) => {
-      const prodCity = product.city;
-      return prodCity.includes(query);
-    });
-  } */
-
-/*   const filteredCitiesProducts = filteredCities(filteredProducts, searchQuery)
- */
   return (
     <div className="theProducts">
       <form action="/" method="get">
@@ -82,29 +69,30 @@ export function AllProductsShow() {
         />
       </form>
 
-
       <h1>Products</h1>
-     {<div className="allProfileProducts">
-        {filterProducts.map((product) => {
-          return (
-            <div className="profileProduct">
-              <Link
-                className="profileProductName"
-                to={`/product/${product._id}`}
-              >
-                <div>
-                  <h3>{product.name}</h3>
-                  <img
-                    className="productImg"
-                    src={product.image}
-                    alt={product.image}
-                  />
-                </div>
-              </Link>
-            </div>
-          );
-        })}
-      </div> }
+      {
+        <div className="allProfileProducts">
+          {filterProducts().map((product) => {
+            return (
+              <div className="profileProduct">
+                <Link
+                  className="profileProductName"
+                  to={`/product/${product._id}`}
+                >
+                  <div>
+                    <h3>{product.name}</h3>
+                    <img
+                      className="productImg"
+                      src={product.image}
+                      alt={product.image}
+                    />
+                  </div>
+                </Link>
+              </div>
+            );
+          })}
+        </div>
+      }
     </div>
   );
 }
