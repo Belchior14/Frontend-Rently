9472c478a4a6a8fa0e408ff0c2c4af308ef4e19b
Belchior <belchior.fontao@gmail.com>
2022-04-30 14:00:21 +0100

    signup CSS

--- src/components/Signup/index.js
+++ src/components/Signup/index.js
@@ -2,7 +2,6 @@ import { AuthContext } from "context";
 import { useContext, useState } from "react";
 import "./signup.css";
 import { Link } from "react-router-dom";
-
 export function SignupForm() {
   const { signup } = useContext(AuthContext);
   const [email, setEmail] = useState("");
@@ -10,58 +9,60 @@ export function SignupForm() {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [username, setUsername] = useState("");
-
   const handleSubmit = (e) => {
     e.preventDefault();
     signup(email, password, firstName, lastName, username);
       };
-
   return (
     <div className="signupDiv">
       <div>
+        <h2>Create an Account</h2>
         <form className="signupForm" onSubmit={handleSubmit}>
-          <label>First Name:</label>
+          <label className="signupInput"></label>
           <input
             type="text"
             id="firstName"
+            placeholder="First Name"
             value={firstName}
             onChange={(e) => {
               setFirstName(e.target.value);
             }}
           />
-          <label>Last Name:</label>
+          <label></label>
           <input
             type="text"
             id="lastName"
+            placeholder="Last Name"
             value={lastName}
             onChange={(e) => {
               setLastName(e.target.value);
             }}
           />
-          <label>Username:</label>
+          <label></label>
           <input
             type="text"
             id="username"
+            placeholder="Username"
             value={username}
             onChange={(e) => {
               setUsername(e.target.value);
             }}
           />
-          <label>Email:</label>
+          <label></label>
           <input
             type="email"
             id="email"
-            placeholder="example@example.com"
+            placeholder="Email"
             value={email}
             onChange={(e) => {
               setEmail(e.target.value);
             }}
           />
-          <label>Password:</label>
+          <label></label>
           <input
             type="password"
             id="password"
-            placeholder="6 characters and contain at least one number, one lowercase and one uppercase letter"
+            placeholder="Password"/* 6 characters and contain at least one number, one lowercase and one uppercase letter */
             value={password}
             onChange={(e) => {
               setPassword(e.target.value);
--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -4,7 +4,6 @@ import "./Signup.css";
 export function Signup () {
     return(
         <div>
-            <h2>Signup</h2>
             <SignupForm/>
         </div>
     )
