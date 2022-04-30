d1d2da8a1ee4befc9312afdf3d41cc10b9b5ce5e
Cristina Quijano <cristina@cristinas-air-2.kpn>
2022-04-30 10:19:12 +0200

    Hero Section

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,21 +1,28 @@
 import "./Home.css";
 import { Link } from "react-router-dom";
+import image from "./heroPicture.png"
 
 export function Home() {
 
   return (
-    <div>
+    <div className="heroSection">
+      <div className="mainQuote">
       <h1 className="h1Home">Rent your favorite 
-      products while spending 
+      <br/>
+      products while spending
+      <br/>
       half of the money</h1>
-
 
       <Link to="/signup"> 
       <button className="Btn">
         Start Renting
       </button>
       </Link>
-   
+      </div>
+
+      <div className="productImages">
+      <img className="heroPicture" src={image} alt="heroPicture" />
+      </div>
 
     </div>
   );
