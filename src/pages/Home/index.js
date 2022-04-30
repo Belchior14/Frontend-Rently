88f5a4eaef349f6188f53c8d8bfa717df7187302
Belchior <belchior.fontao@gmail.com>
2022-04-30 10:50:16 +0100

    home page CSS

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,29 +1,71 @@
 import "./Home.css";
 import { Link } from "react-router-dom";
-import image from "./heroPicture.png"
+import image from "./heroPicture.png";
+import futebolImage from "./futebol.png";
+import technology from "./recording.png";
+import home from "./robot Vacuum.png";
+import leisure from "./leisure.png";
+import books from "./books.png";
+import steps from "./Steps.png";
 
 export function Home() {
-
   return (
-    <div className="heroSection">
-      <div className="mainQuote">
-      <h1 className="h1Home">Rent your favorite 
-      <br/>
-      products while spending
-      <br/>
-      half of the money</h1>
-
-      <Link to="/signup"> 
-      <button className="Btn">
-        Start Renting
-      </button>
-      </Link>
+    <div className="homePage">
+      <div className="heroSection">
+        <div className="mainQuote">
+          <h1 className="h1Home">
+            Rent your favorite
+            <br />
+            products while spending
+            <br />
+            half of the money
+          </h1>
+          <Link to="/signup">
+            <button className="Btn">Start Renting</button>
+          </Link>
+        </div>
+        <div className="productImages">
+          <img className="heroPicture" src={image} alt="heroPicture" />
+        </div>
       </div>
 
-      <div className="productImages">
-      <img className="heroPicture" src={image} alt="heroPicture" />
+      <div className="secondsection">
+        <div className="categories">
+          <div>
+            <h2 className="h2title">Categories</h2>
+          </div>
+          <div className="categoriesExamples">
+            <div className="eachCategory">
+              <h3>Sports</h3>
+              <img className="categoryPics" src={futebolImage} alt="futebol" />
+            </div>
+            <div className="eachCategory">
+              <h3>Technology</h3>
+              <img className="categoryPics" src={technology} alt="Technology" />
+            </div>
+            <div className="eachCategory">
+              <h3>Home</h3>
+              <img className="categoryPics" src={home} alt="Home" />
+            </div>
+            <div className="eachCategory">
+              <h3>Leisure</h3>
+              <img className="categoryPics" src={leisure} alt="Leisure" />
+            </div>
+            <div className="eachCategory">
+              <h3>Others</h3>
+              <img className="categoryPics" src={books} alt="Others" />
+            </div>
+          </div>
+        </div>
       </div>
-
+      <div className="thirdSection">
+        <div>
+          <h2 className="h2title">How to start renting?</h2>
+        </div>
+        <div>
+          <img className="stepsImage" src={steps} alt="steps to rent" />
+        </div>
+      </div>
     </div>
   );
 }
